030868772f9d80beee0aa0509da8b033a4378d43
WeraP123 <prweronika0@gmail.com>
2021-11-22 11:45:14 +0000

    GeneMask

--- POS_GSE18842.R
+++ POS_GSE18842.R
@@ -1,5 +1,6 @@
 library(propOverlap)
 tExpressionData2 <- t(expressionData2)
 Score <- POS(tExpressionData2, CI.emprical(tExpressionData2, Tumor_Control), Tumor_Control)
+Gene.mask <- GMask(tExpressionData2, CI.emprical(tExpressionData2, Tumor_Control) ,Tumor_Control)
 Selection.k <- Sel.Features(tExpressionData2, Tumor_Control)
 Selection.k$Features
