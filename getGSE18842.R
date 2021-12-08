2e91ffe186d307a1f0d1755bf13bcb09525b27f3
WeraP123 <prweronika0@gmail.com>
2021-12-08 00:30:58 +0000

    Innitial commit

--- getGSE18842.R
+++ getGSE18842.R
@@ -1,10 +1,11 @@
 library(GEOquery)
 gset1 <- getGEO("GSE18842", GSEMatrix =TRUE, getGPL=FALSE)
-if (length(gset) > 1) idx <- grep("GPL570", attr(gset1, "names")) else idx <- 1
+if (length(gset1) > 1) idx <- grep("GPL570", attr(gset1, "names")) else idx <- 1
 gset1 <- gset1[[idx]]
-ExpressionDataLun <- exprs(gset)
+ExpressionDataLun <- exprs(gset1)
 boxplot(ExpressionDataLung, outline = FALSE)
 summary(as.vector(ExpressionDataLung))
 hist(ExpressionDataLung, breaks=80, probability = T)
-ClassLung <- gset@phenoData@data[["sample type:ch1"]]
+ClassLung <- gset1@phenoData@data[["sample type:ch1"]]
 ClassLungFactor <- as.factor(ClassLung)
+ExpressionDataLung <- t(ExpressionDataLun)
